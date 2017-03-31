670fe6676758ad4bb383b8344bf5d8825b05ff55
dev201505 <dev201505@163.com>
2017-03-31 09:28:30 +0800

    first commit

---
+++ src/main/java/com/common/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.common;
+
+/**
+ * Created by ouyangyuanming on 2017-3-31.
+ */
+public class HelloWorld {
+    public void sayHello() {
+        System.out.println("hello world!");
+    }
+
+    public static void main(String[] args) {
+        new HelloWorld().sayHello();
+    }
+}
